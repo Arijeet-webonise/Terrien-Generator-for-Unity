c04d2c6d3a23147d6e3ad8e7764d83eeffd831b7
Arijeet Baruah <rishithegamecreator@protonmail.ch>
2016-10-10 22:22:06 +0530

    infinite terrein

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+	public override void OnInspectorGUI(){
+		MapGenerator map = (MapGenerator)target;
+
+		if (DrawDefaultInspector ()) {
+			if (map.autoUpdate) {
+				map.DrawMapInEditor ();
+			}
+		}
+
+		if (GUILayout.Button ("Generate")) {
+			map.DrawMapInEditor ();
+		}
+	}
+}
